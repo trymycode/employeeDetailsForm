e1c0227b44d9c181433d3070da656c8e4fe84648
trymycode <imitasingha1994@gmail.com>
2019-04-08 21:45:29 +0530

    added angular as client side appliation

---
+++ angularApp/src/app/shared/employee.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
+import { HttpClient } from '@angular/common/http';
+import {Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+}
